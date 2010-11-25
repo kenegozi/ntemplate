07d5337d577371b6ac2fc5ba14ce3113d09d7bd0
kenegozi <mail@kenegozi.com>
2010-11-25 23:51:02 +0200

    include SolutionAssemblyInfo in projects

--- src/NTemplate/Properties/AssemblyInfo.cs
+++ src/NTemplate/Properties/AssemblyInfo.cs
@@ -1,10 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Ken Egozi")]
 [assembly: AssemblyProduct("NTemplate")]
 [assembly: AssemblyCopyright("Copyright © Ken Egozi 2010")]
-
-[assembly: AssemblyVersion("0.9.0.*")]
