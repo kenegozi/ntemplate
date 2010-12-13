e5fd40274399e3ac0744c3952676c659c6511967
kenegozi <mail@kenegozi.com>
2010-12-13 16:00:42 +0200

    better handling page directive

--- src/NTemplate/Compiler/PreCompiler/Steps/ExtractPageDirectiveStep.cs
+++ src/NTemplate/Compiler/PreCompiler/Steps/ExtractPageDirectiveStep.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NTemplate.Compiler.Directives;
 
 namespace NTemplate.Compiler.PreCompiler.Steps
 {
 	public class ExtractPageDirectiveStep : IPreCompilationStep
 	{
+		static readonly string BaseViewTypeExpression = @"(?<base>[\w.]+)(?:(?:<(?<view>[\w.<>]+)>)|(?:`1\[(?<view>[\w.`\[\]]+)\]))";
+		static readonly Regex PageDirective = new Regex(
+@"^\s*<%@\s*Page\s+Language\s*=\s*""c#""(?:\s+Inherits\s*=\s*""" + BaseViewTypeExpression + @"?\s*"")?.*%>\s*$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
 		public void Execute(TemplateCompilationInfo templateCompilationInfo)
 		{
 			var pageDirectiveLine = templateCompilationInfo.OriginalLines
 				.Where(l => l.Processed == false)
-				.Where(l => l.Content.StartsWith("<%@page", StringComparison.InvariantCultureIgnoreCase))
+				.Where(l => PageDirective.IsMatch(l.Content))
 				.FirstOrDefault();
 
 			if (pageDirectiveLine == null) return;
